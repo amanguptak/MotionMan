3ff29f16e7c1132aa3219947563c92d1f1abf266
Aman Gupta <eagleax7@gmail.com>
2023-11-29 16:26:18 +0530

    login, sing-up

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,9 @@
 import { SignIn } from "@clerk/nextjs";
  
 export default function Page() {
-  return <SignIn />;
+  return (
+    <div className=" bg-[#a2c4c9] flex h-screen items-center justify-center">
+        <SignIn appearance= {{variables:{colorPrimary:"#0072b1"}}} />
+    </div>
+  )
 }
--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,9 @@
 import { SignUp } from "@clerk/nextjs";
  
 export default function Page() {
-  return <SignUp />;
+    return (
+        <div className="flex h-screen items-center justify-center bg-[#a2c4c9]">
+            <SignUp appearance= {{variables:{colorPrimary:"#0072b1"}}} />
+        </div>
+      )
 }

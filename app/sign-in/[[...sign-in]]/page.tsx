641973a49e02783c2c88e85c35b830f47cd9c6d6
Aman Gupta <eagleax7@gmail.com>
2023-11-29 19:27:36 +0530

    logo

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,8 +1,14 @@
 import { SignIn } from "@clerk/nextjs";
- 
+ import img from "@/public/images/motion.png"
+ import Image from "next/image"
+
 export default function Page() {
   return (
-    <div className=" bg-[#a2c4c9] flex h-screen items-center justify-center">
+    <div className=" bg-[#a2c4c9] flex flex-col h-screen items-center justify-center">
+   <Image src={img} alt="logo" height={200} width={200}/>
+
+
+
         <SignIn appearance= {{variables:{colorPrimary:"#0072b1"}}} />
     </div>
   )

fb4cba7fe89cc23054fceda4ccb71943aef667ff
Aman Gupta <eagleax7@gmail.com>
2024-04-11 11:20:41 +0530

    chat-bug-fixed

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
   const {userId} = auth()
   if(userId){redirect("/motion")}
   return (
-   <main className="flex  items-center justify-center h-screen lg:gap-4 flex-col bg-[#a2c4c9]">
+   <main className="flex  items-center justify-center h-screen lg:gap-4 flex-col ">
     <div className="grid place-items-center p-4 gap-4 lg:max-w-3xl lg:grid-flow-col">
     <Image src={logo} alt="logo" height={300} width={300} />
     <p className="tracking-tight text-sm  lg:text-md text-[#fefde6] font-medium">Discover Motion: Your Note-Taking Companion, now elevated with AI precision. Effortlessly capture ideas, and watch as Motion transforms them into polished notes. Seamlessly organized, profoundly professional. Your thoughts, refined. :- Made By Aman Gupta</p>
